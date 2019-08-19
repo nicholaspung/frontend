5ce330d1dbc8f92e8c3bacf4058faa904eb5d425
Ryan Boris <ryan.boris@gmail.com>
2019-08-19 06:15:43 -0400

    :rotating_light: Add correct airbnb config for precommit hook

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       users: [],
       devURL: 'http://localhost:5000',
       prodURL: 'https://lambdanextbackend.herokuapp.com'
-    }
+    };
   }
 
   async componentDidMount() {
-    const { devURL, prodURL } = this.state
+    const { devURL, prodURL } = this.state;
     try {
-      const { data } = await axios.get(`${prodURL}/users`)
-      this.setState({ users: data })
+      const { data } = await axios.get(`${prodURL}/users`);
+      this.setState({ users: data });
     } catch (err) {
-      console.error({ message: err.message })
+      console.error({ message: err.message });
     }
   }
 
   render() {
-    const { users } = this.state
+    const { users } = this.state;
     return (
       <div>
         <h1>Testing Frontend/Server/Database connection...</h1>
-        {users.map(user => {
-          return (
-            <div key={user.id}>
-              <p>{user.username}</p>
-              <p>{user.email}</p>
-            </div>
-          )
-        })}
+        {users.map((user) => (
+          <div key={user.id}>
+            <p>{user.username}</p>
+            <p>{user.email}</p>
+          </div>
+        ))}
       </div>
-    )
+    );
   }
 }
