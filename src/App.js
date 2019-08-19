d29e4232bd6a31beafba52f141f9a299c2b3e683
Ryan Boris <ryan.boris@gmail.com>
2019-08-19 04:15:45 -0400

    :construction: Create app to test db/server connections

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class App extends Component {
+  state = {
+    users: [],
+    devURL: 'http://localhost:5000'
+  }
+
+  async componentDidMount() {
+    const { devURL } = this.state
+    try {
+      const { data } = await axios.get(`${devURL}/users`)
+      this.setState({ users: data })
+    } catch (err) {
+      console.error({ message: err.message })
+    }
+  }
+
   render() {
+    const { users } = this.state
     return (
-      <div>
-        <p>App.js renders.</p>
+      <div className="testRun">
+        <h1>Testing Frontend/Server/Database connection...</h1>
+        {users.map(user => {
+          return (
+            <div key={user.id}>
+              <p>{user.username}</p>
+              <p>{user.email}</p>
+            </div>
+          )
+        })}
       </div>
     )
   }
