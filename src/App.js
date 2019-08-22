661f6c262aaae2b3889f1d37d21a0239c75f3fe8
George Adams <gadams@Georges-MacBook-Air.local>
2019-08-22 11:56:27 -0500

    problem description  setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import PorblemCard from './components/ProblemCard';
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     const { users } = this.state;
     return (
