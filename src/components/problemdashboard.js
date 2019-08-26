c14c7a9d9d928e4ab8cb924920e1f829213843b6
Nicholas Pung <nckpng95@gmail.com>
2019-08-26 08:20:41 -0700

    Removed commented out CSS

--- src/components/problemdashboard.js
+++ src/components/problemdashboard.js
@@ -1,26 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
-// import Card from '@material-ui/core/Card';
-// import CardHeader from '@material-ui/core/CardHeader';
-// import CardMedia from '@material-ui/core/CardMedia';
-// import CardContent from '@material-ui/core/CardContent';
-// import CardActions from '@material-ui/core/CardActions';
-// import Avatar from '@material-ui/core/Avatar';
-// import IconButton from '@material-ui/core/IconButton';
-// import Typography from '@material-ui/core/Typography';
-// import FavoriteIcon from '@material-ui/icons/Favorite';
-// import ShareIcon from '@material-ui/icons/Share';
-// import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-// import MoreVertIcon from '@material-ui/icons/MoreVert';
 import Grid from '@material-ui/core/Grid';
-
 import { styled } from '@material-ui/styles';
 
 import { getProblems } from '../actions';
 import ProblemCard from './ProblemCard'
-
-
 
 const MyGrid = styled(Grid)({
   display: 'flex',
@@ -28,8 +12,6 @@ const MyGrid = styled(Grid)({
   justifyContent: 'space-between',
   marginTop:50
 });
-
-
 
 class ProblemDashboard extends React.Component {
   
