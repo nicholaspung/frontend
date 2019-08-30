cae6df66cf629ab680102b74d0624727b069221c
George Adams <gadams@Georges-MacBook-Air.local>
2019-08-30 14:40:42 -0500

    pushing again

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Container from '@material-ui/core/Container';
-import Header from './Header';
-import HomePage from './HomePage';
-import About from './About';
-import HowItWorks from './HowItWorks';
-import Footer from './Footer';
-import ProblemDashboard from './problemdashboard';
-import ProblemCard from './ProblemCard';
-import DetailsPage from './DetailsPage';
-import ProblemSubmission from './ProblemSubmission';
-
+import React from "react";
+import { Route } from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
+import Header from "./Header";
+import HomePage from "./HomePage";
+import About from "./About";
+import HowItWorks from "./HowItWorks";
+import Footer from "./Footer";
+import ProblemDashboard from "./problemdashboard";
+import ProblemCard from "./ProblemCard";
+import DetailsPage from "./DetailsPage";
+import ProblemSubmission from "./ProblemSubmission";
+//s
 
 export default function App() {
   return (
