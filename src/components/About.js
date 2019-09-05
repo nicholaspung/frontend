9a81070063134874c09c69e8b03fe4ea21c4b53a
Nicholas Pung <nckpng95@gmail.com>
2019-09-05 15:02:51 -0700

    Styled AboutUs page, stil needs work

--- src/components/About.js
+++ src/components/About.js
@@ -1,66 +1,174 @@
 import React from "react";
-import team from "../static/team";
-import TeamPictures from "./TeamPictures";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
-import { styled } from "@material-ui/styles";
+import Typography from "@material-ui/core/Typography";
+import CardMedia from "@material-ui/core/CardMedia";
+import { useTheme, styled } from "@material-ui/styles";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import SearchIcon from "@material-ui/icons/Search";
+import HowToRegIcon from "@material-ui/icons/HowToReg";
+import DoneAllIcon from "@material-ui/icons/DoneAll";
+import team from "../static/team";
+import TeamPictures from "./TeamPictures";
+import aboutus from "../static/images/marketing/teamwork.jpg";
 
-const MyPaper = styled(Paper)({
-  fontSize: "3rem",
-  margin: "1rem"
+const HowItWorks = styled(Paper)({
+  margin: "20px"
 });
 
-const About = () => (
-  <Grid container justify="center">
-    <MyPaper>About Us</MyPaper>
-    <Paper>
-      Lambda School NeXt is Lambda School's flagship program to help their
-      students build a fully functional project to showcase their skills. The
-      goal of Lambda School NeXt is to build projects that solve a real world
-      problem. This website was created to help alleviate the pain of choosing
-      which problem to solve by helping source volunteers and research the
-      problem before committing teams to solve the problem.
-    </Paper>
-    <Grid>
-      <p>HowItWorks</p>
-      <p>Step 1</p>
-      <p>Find a problem that you have.</p>
-      <p>Step 2</p>
-      <p>
-        Read the description. See if you want someone to solve your problem.
-      </p>
-      <p>Step 3</p>
-      <p>
-        Sign up to volunteer your time to help create a solution. Your time will
-        only be used to help create a project out of a problem in the beginnings
-        stages.
-      </p>
-      <p>Step 4</p>
-      <p>
-        Lambda School students will reach out to ask for help. Afterwards, they
-        will start on building a project that solves your problem.
-      </p>
-      <p>Step 5</p>
-      <p>Let us know if you would like to be notified about feedback.</p>
-      <p>Step 6</p>
-      <p>
-        After 8 weeks, a working solution will be posted up for you to check out
-        our Lambda School students' work!
-      </p>
+const BoldTypography = styled(Typography)({
+  fontWeight: "bold"
+});
+
+const PaddedBottomTypography = styled(Typography)({
+  paddingBottom: '5px'
+})
+
+const About = () => {
+  const theme = useTheme();
+  return (
+    <Grid container justify="center">
+      <Grid
+        container
+        justify="center"
+        alignItems="center"
+        direction="row-reverse"
+      >
+        <Grid item lg={6} sm={8}>
+          <CardMedia
+            component="img"
+            alt="Person who has a problem, thinks about it, and then solves it."
+            height="auto"
+            src={aboutus}
+            title="Contemplative Person"
+          />
+        </Grid>
+        <Grid item lg={4} xs={10} style={{ paddingBottom: "20px" }}>
+          <BoldTypography
+            variant="h4"
+            component="h2"
+            style={{ textAlign: "center" }}
+          >
+            About Us
+          </BoldTypography>
+          <Typography
+            variant="body1"
+            component="p"
+            style={{ textAlign: "center" }}
+          >
+            Lambda School NeXt is Lambda School's flagship program to help their
+            students build a fully functional project to showcase their skills.
+            The goal of Lambda School NeXt is to build projects that solve a
+            real world problem. This website was created to help alleviate the
+            pain of choosing which problem to solve by helping source volunteers
+            and research the problem before committing teams to solve the
+            problem.
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        justify="center"
+        style={{ backgroundColor: theme.palette.background.secondary }}
+      >
+        <HowItWorks square>
+          <Grid container justify="center" style={{ paddingTop: "10px" }}>
+            <BoldTypography variant="h4" component="h2">
+              How It Works
+            </BoldTypography>
+          </Grid>
+          <Grid
+            container
+            alignItems="center"
+            justify="center"
+            style={{ backgroundColor: theme.palette.background.secondary }}
+          >
+            <Grid item xs={1}>
+              <SearchIcon fontSize="large" />
+            </Grid>
+            <Grid item xs={8}>
+              <BoldTypography variant="body2" component="p">
+                Step 1
+              </BoldTypography>
+              <PaddedBottomTypography variant="body1" component="p">
+                Find a problem that you have.
+              </PaddedBottomTypography>
+            </Grid>
+          </Grid>
+          <Grid container alignItems="center" justify="center">
+            <Grid item xs={1}>
+              <SubscriptionsIcon fontSize="large" />
+            </Grid>
+            <Grid item xs={8}>
+              <BoldTypography variant="body2" component="p">
+                Step 2
+              </BoldTypography>
+              <PaddedBottomTypography variant="body1" component="p">
+                Sign up to a problem, and when enough participants are
+                registered, we'll turn the problem into a project.
+              </PaddedBottomTypography>
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            justify="center"
+            alignItems="center"
+            style={{ backgroundColor: theme.palette.background.secondary }}
+          >
+            <Grid item xs={1}>
+              <HowToRegIcon fontSize="large" />
+            </Grid>
+            <Grid item xs={8}>
+              <BoldTypography variant="body2" component="p">
+                Step 3
+              </BoldTypography>
+              <PaddedBottomTypography variant="body1" component="p">
+                Lambda School students will reach out to ask for help.
+                Afterwards, they will start building the project that solves
+                your problem.
+              </PaddedBottomTypography>
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            justify="center"
+            alignItems="center"
+            style={{ paddingBotton: "10px" }}
+          >
+            <Grid item xs={1}>
+              <DoneAllIcon fontSize="large" />
+            </Grid>
+            <Grid item xs={8}>
+              <BoldTypography variant="body2" component="p">
+                Step 4
+              </BoldTypography>
+              <PaddedBottomTypography variant="body1" component="p">
+                After 8 weeks, Lambda School students have finished Version 1.0
+                of the project! You'll be able to try out the project as soon as
+                they are finished.
+              </PaddedBottomTypography>
+            </Grid>
+          </Grid>
+        </HowItWorks>
+      </Grid>
+      <Grid container justify="center">
+        <Typography variant="h6" component="h2">
+          Meet the team
+        </Typography>
+      </Grid>
+      <Grid container spacing={3} justify="center">
+        {team.map(person => (
+          <TeamPictures
+            name={person.name}
+            description={person.description}
+            github={person.github}
+            linkedin={person.linkedin}
+            key={person.name}
+          />
+        ))}
+      </Grid>
     </Grid>
-    <MyPaper>Meet the team</MyPaper>
-    <Grid container spacing={3} justify="center">
-      {team.map(person => (
-        <TeamPictures
-          name={person.name}
-          description={person.description}
-          github={person.github}
-          linkedin={person.linkedin}
-          key={person.name}
-        />
-      ))}
-    </Grid>
-  </Grid>
-);
+  );
+};
 
 export default About;
