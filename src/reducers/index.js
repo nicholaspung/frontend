5d18cd7c2164d5a337b5da6436b4fef14288e493
Nicholas Pung <nckpng95@gmail.com>
2019-09-09 09:25:01 -0700

    Fixed nav bug

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import problems from "./problems";
 import users from "./users";
+import nav from "./nav";
 
 const reducer = combineReducers({
   problems,
-  users
+  users,
+  nav
 });
 
 export default reducer;
