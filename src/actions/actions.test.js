7dad8840f45a90419809beb8127a6add48bd002d
Nicholas Pung <nckpng95@gmail.com>
2019-09-17 10:51:38 -0700

    Added tests for nav actions

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -8,4 +8,30 @@ describe("set header nav", () => {
     };
     expect(actions.setHeaderNavFalse()).toEqual(expectedAction);
   });
+
+  it("should create an action to set header nav true", () => {
+    const expectedAction = {
+      type: actions.SET_HEADER_NAV_TRUE,
+      payload: true
+    };
+    expect(actions.setHeaderNavTrue()).toEqual(expectedAction);
+  });
+
+  it("should create an action to set header nav opposite true", () => {
+    const checked = true;
+    const expectedAction = {
+      type: actions.SET_HEADER_NAV_OPPOSITE,
+      payload: false
+    };
+    expect(actions.setHeaderNavOpposite(checked)).toEqual(expectedAction);
+  });
+
+  it("should create an action to set header nav opposite false", () => {
+    const checked = false;
+    const expectedAction = {
+      type: actions.SET_HEADER_NAV_OPPOSITE,
+      payload: true
+    };
+    expect(actions.setHeaderNavOpposite(checked)).toEqual(expectedAction);
+  });
 });
